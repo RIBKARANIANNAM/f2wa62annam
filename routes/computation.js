882a9536723b58dd0b6933d47c215b1447b7fa34
RIBKARANIANNAM <aribkarani2022@gmail.com>
2022-10-26 18:39:07 -0500

    new endpoint/computation

--- routes/computation.js
+++ routes/computation.js
@@ -10,13 +10,13 @@ router.get('/',function(req,res,next)
   if (x==undefined){
   x=y;
   }
-  res.render ('computation',{values: `f[Math.pow] applied to (${x}) is ${Math.pow(x)}`})
+  res.render ('computation',{values: `f[Math.sign] applied to (${x}) is ${Math.sign(x)}`})
 
 }
 )
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render ('computation',{values: `f[Math.pow] applied to (${x}) is ${Math.pow(x)}`})
+  res.render ('computation',{values: `f[Math.sign] applied to (${x}) is ${Math.sign(x)}`})
 
 res.end();
 });
